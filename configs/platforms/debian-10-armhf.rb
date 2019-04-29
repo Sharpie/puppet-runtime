144cd3a1a1454722a3cffaab59b7f87ae10a5d44
Charlie Sharpsteen <source@sharpsteen.net>
2019-04-28 21:48:50 -0700

    Add debain-9-armhf and debian-10-armhf
    This commit adds armhf plaform definitions for Stretch and Buster based on
    debian-10-amd64 and Mike Stahnke's work in:
    
      https://github.com/puppetlabs/puppet-agent/pull/1362

---
+++ configs/platforms/debian-10-armhf.rb
@@ -0,0 +1,54 @@
+platform "debian-10-armhf" do |plat|
+  plat.servicedir "/lib/systemd/system"
+  plat.defaultdir "/etc/default"
+  plat.servicetype "systemd"
+  plat.codename "buster"
+  plat.platform_triple "arm-linux-gnueabihf"
+
+  plat.install_build_dependencies_with "DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends "
+
+  # STORY TIME: In order to keep on-disk sizes low, most docker containers
+  #             are configured to skip installation of man pages. This
+  #             generally works out fine except the post-install script
+  #             of `openjdk-11-jdk-headless` assumes noone would ever do
+  #             such a thing and fails.
+  plat.provision_with 'mkdir -p /usr/share/man/man1'
+  plat.provision_with "dpkg --add-architecture #{plat.get_architecture}"
+
+  packages = [
+    'cmake',
+    "crossbuild-essential-#{plat.get_architecture}",
+    'debhelper',
+    'devscripts',
+    'fakeroot',
+    "libc6-dev:#{plat.get_architecture}",
+    "libbz2-dev:#{plat.get_architecture}",
+    "libreadline-dev:#{plat.get_architecture}",
+    'make',
+    'openjdk-11-jdk-headless',
+    'pkg-config',
+    'quilt',
+    'rsync',
+    "zlib1g-dev:#{plat.get_architecture}",
+  ]
+  plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends #{packages.join(' ')}"
+
+  plat.cross_compiled "true"
+  plat.output_dir File.join("deb", plat.get_codename, "PC1")
+
+  plat.vmpooler_template "debian-10-x86_64"
+
+  # NOTE: Bring your own image. The image is expected to satisfy the following
+  #       conditions:
+  #
+  #         - Runs SystemD
+  #         - Runs SSHD under SystemD
+  #           - SSHD allows pubkey access to the root user via a
+  #             key set by the VANAGON_SSH_KEY environment variable.
+  plat.docker_image ENV['VANAGON_DOCKER_IMAGE']
+  plat.ssh_port 4222
+  plat.docker_run_args ['-t', # TTY makes SystemD messages available to `docker logs`
+                        '--tmpfs=/run',
+                        '--tmpfs=/run/lock',
+                        '--volume=/sys/fs/cgroup:/sys/fs/cgroup:ro']
+end
