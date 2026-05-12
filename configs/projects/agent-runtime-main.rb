14ca716d964120c15733e24aa4694b9c058aa9ae
Tim Meusel <tim@bastelfreak.de>
2026-05-12 17:05:02 +0200

    openvox 9: Remove rubygem-multi_json
    We agreed on removing it:
    https://github.com/OpenVoxProject/openvox/pull/293
    
    Signed-off-by: Tim Meusel <tim@bastelfreak.de>

--- configs/projects/agent-runtime-main.rb
+++ configs/projects/agent-runtime-main.rb
@@ -49,7 +49,6 @@ project 'agent-runtime-main' do |proj|
   proj.component 'rubygem-highline'
   proj.component 'rubygem-hocon'
   proj.component 'rubygem-locale'
-  proj.component 'rubygem-multi_json' # TODO: obsolete for openvox 9 - https://github.com/OpenVoxProject/openvox/pull/293
   proj.component 'rubygem-net-ssh'
   proj.component 'rubygem-optimist'
   proj.component 'rubygem-semantic_puppet'
